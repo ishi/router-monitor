3a1b51bac836c2171f5a9f6492a5335bc1536ea1
Radoslaw Zielinski <radzioshp@gmail.com>
2012-01-06 13:28:02 +0000

    Poprawienie wyświetlania komunikatów

--- app/assets/javascripts/default.js
+++ app/assets/javascripts/default.js
@@ -3,4 +3,11 @@ $(function() {
 	$('.button').button();
 	// tworzymy menu po lewej
 	$('#menu').sideMenu();
+	// chowamy komunikaty
+	var intervalStep = 5000, hideInterval = intervalStep;
+	$('.ui-message').each(function() {
+		$(this).click(function() { $(this).clearQueue().fadeOut(); })
+				.delay(hideInterval).fadeOut();
+		hideInterval += intervalStep;
+	})
 });
