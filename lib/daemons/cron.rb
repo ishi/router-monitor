8de0e639d20732ef70eabbd7dde3ff53d53bf92c
Radosław Zieliński <radoslaw@zielinski.cc>
2012-01-06 02:55:11 +0100

    Poprawienie skryptu crona, dodano logowanie ERROR w przypadku błędu skryptu, poprawiono mechanizm uruchamiania skryptów

--- lib/daemons/cron.rb
+++ lib/daemons/cron.rb
@@ -1,6 +1,22 @@
 #!/usr/bin/env ruby
 # encoding: UTF-8
 
+# RAILS_ROOT = File.dirname(File.dirname(__FILE__))
+# RAILS_ENV = File.exist?(File.join(RAILS_ROOT, 'development.env')) && 'development' || 'production'
+# 
+# require 'rubygems'
+# require "daemons"
+# require 'yaml'
+# require 'erb'
+# 
+# gem 'activesupport', '>=3.0.0.beta4'
+# require 'active_support'
+# 
+# # For some reason, ActiveSupport 3.0.0 doesn't load.
+# # Load needed extension directly for now.
+# require "active_support/core_ext/object"
+# require "active_support/core_ext/hash"
+
 # You might want to change this
 ENV["RAILS_ENV"] ||= "production"
 
@@ -8,33 +24,31 @@ require File.expand_path('../../../config/application.rb', __FILE__)
 Rails.application.require_environment!
 require 'open4'
 
-module CRON
-	class CronLogger < Logger
-		def format_message(severity, timestamp, progname, msg)
-			"#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n" 
-		end 
-	end
-end
-
-logfile = File.open("#{Rails.root}/log/cron.log", 'a')
-logfile.sync = true
-Rails.logger = CRON::CronLogger.new(logfile)
-Rails.logger.level = Logger::INFO unless 'development' == Rails.env
-
-Rails.logger.debug "Uruchamiam server w środowisu #{Rails.env}"
 
 module CRON
-
+  SCRIPT_PATH = File.join(Rails.root, 'tmp', 'scripts')
+  
+  
+  class CronLogger < Logger
+    def format_message(severity, timestamp, progname, msg)
+      "#{timestamp.to_formatted_s(:db)} #{severity} #{msg}\n" 
+    end 
+  end
+  
 	class CronJob
 		attr_accessor :job
 
 		def initialize(job)
-			self.job = job
+			@job = job
+			@jobFile = Tempfile.new([@job.name, '.tmp.sh'], SCRIPT_PATH)
+			@jobFile.chmod(0500)
+			@jobFile.write(@job.script.gsub(/\r/, ''))
+			@jobFile.close
 		end
 
 		def call job
 			Rails.logger.info "Wykonuję zadanie #{@job.name} co #{@job.interval}s"
-			pid, stdin, stdout, stderr = Open4::popen4 @job.script
+			pid, stdin, stdout, stderr = Open4::popen4 "sh -c #{@jobFile.path}"
 			stdin.close
 
 			ignored, status = ::Process::waitpid2 pid
@@ -42,12 +56,13 @@ module CRON
 			value = stdout.read.strip
 			Rails.logger.debug "[TASK] #{@job.name} pid        : #{ pid }"
 			Rails.logger.debug "[TASK] stdout     : #{ value }"
-			Rails.logger.debug "[TASK] stderr     : #{ stderr.read.strip }"
 			Rails.logger.debug "[TASK] status     : #{ status.inspect }"
 			Rails.logger.debug "[TASK] exitstatus : #{ status.exitstatus }"
 			if status.exitstatus.zero?
 				Rails.logger.debug "[TASK] Dodaje wartość #{ value } do bazy rrd"
 				@job.update_rrd value
+			else
+        Rails.logger.error "[TASK] Błąd podczas wykonywania zadania: #{ stderr.read.strip }"
 			end
 		rescue Exception => e
 			Rails.logger.error "Wyjątek podczas wykonywania skryptu #{@job.name}: #{e}"
@@ -91,9 +106,16 @@ module CRON
 
 	end
 
+  logfile = File.open("#{Rails.root}/log/cron.log", 'a')
+  logfile.sync = true
+  Rails.logger = CronLogger.new(logfile)
+  Rails.logger.level = Logger::INFO unless 'development' == Rails.env
+  
+  Rails.logger.debug "Uruchamiam server w środowisu #{Rails.env}"
+  
+  Dir.mkdir SCRIPT_PATH unless File.directory? SCRIPT_PATH
 
-	Rails.logger.info 'Zaczynam nasłuchiwać'
-
+  Rails.logger.info 'Zaczynam nasłuchiwać'
 	EM.run do
 		Signal.trap("INT")  { EM.stop }
 		Signal.trap("TERM") { EM.stop }
@@ -101,6 +123,5 @@ module CRON
 		StatisticValue.all.each { |v| Cron.add v }
 		server = EM.start_server("127.0.0.1", 22222, Cron)
 	end
-
-	Rails.logger.info 'Bye...'
+  Rails.logger.info 'Bye...'
 end
