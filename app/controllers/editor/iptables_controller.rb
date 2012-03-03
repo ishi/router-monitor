fce222d227188617c1bc46e889708cde4cc0b17d
Radoslaw Zielinski <radzioshp@gmail.com>
2012-03-03 23:06:48 +0000

    Zapisywanie definicji iptables spowrotem do plików

--- app/controllers/editor/iptables_controller.rb
+++ app/controllers/editor/iptables_controller.rb
@@ -1,3 +1,5 @@
+#encoding: UTF-8
+
 class Editor::IptablesController < ApplicationController
   def show
     redirect_to :action => :edit
@@ -5,19 +7,35 @@ class Editor::IptablesController < ApplicationController
 
   def edit
     @rules = Hash.new { |h,k| h[k] = Hash.new { |h,k| h[k] = Array.new } }
-    dir = File.join('/app', 'etc', 'epp')
-    Dir.foreach(dir) do |filename|
+    Dir.foreach(conf_dir) do |filename|
       next if (File.directory?(filename) || (/[^.]\.[^.]/ =~ filename).nil?)
       
       interface, chain = filename.split /\./
-      @rules[interface.to_sym][chain.to_sym] += (File.open(File.join(dir, filename)).readlines.map { |v| v.strip })
+      @rules[interface.to_sym][chain.to_sym] += (File.open(File.join(conf_dir, filename)).readlines.map { |v| v.strip })
     end
-    puts @rules
   end
 
   def update
     @rules = params[:rules]
-    render :edit
+    return render :edit, :flash => { :error => 'Brak danych do zapisu' } if @rules.blank?
+    
+    @rules.each do |interface, chains|
+      chains.each do |chain, rules|
+        filename = File.join(conf_dir, "#{interface}.#{chain}")
+        File.open(filename, 'w') {|f| f.write(rules.join "\n") }
+      end
+    end
+    
+    flash[:notice] = 'Reguły zostały zapisane'
+    redirect_to :action => 'edit'
+  rescue
+    return render :edit, :flash => { :error => "Nie udało się zapisać pliku #{filename}" }
+  end
+  
+  private
+  
+  def conf_dir
+    @dir ||= File.join('/app', 'etc', 'epp')
   end
 
 end
