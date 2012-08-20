448ce633fba82793c705ac1a2ac5c570c887517c
ishi <kontakt@radekzielinski.pl>
2012-08-20 23:29:29 +0200

    Zapisywanie konfiguracji

--- www/app/assets/javascripts/wizard/zones.js.coffee
+++ www/app/assets/javascripts/wizard/zones.js.coffee
@@ -7,18 +7,20 @@ $(document).ready ->
 
   $('a[data-action="add"]').on 'click', ->
     dialog.dialog('open')
+    $('#zone_old_name', form).val('')
 
   $('a[data-action="edit"]').on 'click', ->
     form = $('div#add-form')
     data = $('table#add-destination .ui-state-hover:first').parent().children('.ui-widget-content')
     data.each ->
       $el = $(this)
+      $('#zone_old_name', form).val($el.text()) if $el.data('name') == 'name'
       $('#zone_' + $el.data('name'), form).val($el.text())
     $('select#zone_method').change()
     dialog.dialog('open')
 
   $('select#zone_method').on 'change keyup', ->
     if $(this).val() == 'DHCP'
-      $('input[data-disable-on*="method-dhcp"]').prop('disabled', true)
+      $('input[data-disable-on*="method-dhcp"]').prop('readonly', true)
     else
-      $('input[data-disable-on*="method-dhcp"]').prop('disabled', false)
+      $('input[data-disable-on*="method-dhcp"]').prop('readonly', false)
--- www/app/models/zone.rb
+++ www/app/models/zone.rb
@@ -6,7 +6,7 @@ class Zone
   Interfaces_path='/app/etc/interfaces.test'
   @persisted = false
 
-  attr_accessor :name, :type, :method, :ip, :mask, :gateway, :dns, :interface
+  attr_accessor :name, :old_name, :type, :method, :ip, :mask, :gateway, :dns, :interface
   validates :name, :presence => { :message => 'Podaj nazwe strefy' }
   validates :ip, :presence => { :message => 'Podaj IP dla strefy' }, :unless => :method_dhcp?
   validates :mask, :presence => { :message => 'Podaj maske dla strefy' }, :unless => :method_dhcp?
@@ -15,6 +15,10 @@ class Zone
     attributes.each do |name, value|
       send("#{name}=", value)
     end
+  end
+  
+  def old_name
+    @old_name || name
   end
 
   def method
@@ -26,12 +30,71 @@ class Zone
   end
 
   def save
-    return false unless valid?
-    return true
+    replace_definition_in_file if valid?
+    Rails.logger.debug get_file_content.join
   end
 
   def method_dhcp?
     'DHCP' == method
+  end
+
+  private
+
+  def get_file_content
+    @interfaces_content ||= self.class.get_interfaces_content
+  end
+
+  def replace_definition_in_file
+    remove_interface_mapping && remove_zone_definition && add_interface_mapping && add_zone_definition
+  end
+
+  def remove_zone_definition
+    remove_stanza(/^\s*iface\s+(#{zone_name_to_save(old: true)}|#{old_name})\s+/)
+  end
+
+  def remove_interface_mapping
+    remove_stanza(/^\s*(mapping\s+#{interface}|auto\s+#{interface}|auto\s+#{old_name})\s*$/)
+  end
+
+  def remove_stanza pattern
+    delete = false
+    get_file_content.delete_if do | line |
+      delete &= !self.class.stanza_line?(line)
+      delete |= line =~ pattern
+    end
+  end
+
+  def add_zone_definition
+    content = get_file_content
+    content << "iface #{zone_name_to_save} inet #{method.downcase}\n"
+    content << "#" if method_dhcp?
+    content << "address #{ip}\n"
+    content << "#" if method_dhcp?
+    content << "netmask #{mask}\n"
+    content << "#" if method_dhcp?
+    content << "#network 153.19.250.128\n"
+    content << "#broadcast 153.19.250.143\n"
+    content << "#" if method_dhcp?
+    content << "gateway #{gateway}\n"
+    content << "#" if method_dhcp?
+    content << "dns-nameservers #{dns}\n"
+    content << "#dns-search\n"
+    content << "#hwaddress ether 00:01:02:a5:10:10\n"
+    content << "\n"
+  end
+
+  def add_interface_mapping
+    return true if interface.blank?
+    get_file_content.concat [
+      "mapping #{interface}\n",
+      "\tscript /app/etc/zone-interface-mapping.sh\n",
+      "\tmap #{zone_name_to_save}\n",
+      "\n"
+    ]
+  end
+
+  def zone_name_to_save(params = {})
+    "#{(params[:old] ? old_name : name).gsub(/ /, '_')}-#{type}"
   end
 
   class << self
@@ -40,7 +103,7 @@ class Zone
         zones = {}
         zone = nil
         new_zone = false
-        interface_file_lines = File.readlines(Interfaces_path)
+        interface_file_lines = get_interfaces_content
 
         while line = interface_file_lines.shift do
           zone = nil if stanza_line? line
@@ -67,7 +130,9 @@ class Zone
       @interfaces ||= [''] + `ip l | grep -ve '^\s' | cut -d' ' -f 2 |  tr ':\n' ' '`.split(/\s+/)
     end
 
-    private
+    def get_interfaces_content
+      File.readlines(Interfaces_path)
+    end
 
     def stanza_line? line
       line =~ /^\s*(auto\s+|allow-|iface\s+|mapping\s+|source\s+)/
@@ -91,15 +156,16 @@ class Zone
     end
 
     def parse_address_lines line, interface_file_lines, zones
-      Rails.logger.debug line =~ /^\s*iface\s+([^\s]+)\s+\w+\s+(\w+)\s*$/
+      line =~ /^\s*iface\s+([^\s]+)\s+\w+\s+(\w+)\s*$/
       method = $2.upcase
       name, type = $1.gsub(/_/, ' ').split '-'
       find_or_create_zone zones, {:name => name, :type => type, :method => method}
     end
 
+    private
+
     def find_or_create_zone zones, params
-      return nil if !params[:name]
-      zones[:"#{params[:name]}"] ||= Zone.new(params)
+      zones[:"#{params[:name]}"] ||= Zone.new(params) unless !params[:name]
     end
   end
 end
